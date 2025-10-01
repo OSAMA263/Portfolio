3d01195e33cfb0107451c26e55074296107a4951
osama <osamaelseify2@gmail.com>
2025-10-01 10:02:10 +0300

    change hom page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,8 @@ import PageWrapper from "../../shared/PageWrapper";
 import SectionHeader from "../../shared/SectionHeader";
 import FormWrapper from "./sections/FormWrapper";
 import { FaLinkedin, FaFacebook, FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { AiOutlineDownload } from "react-icons/ai";
 
 export default function Contact() {
   return (
@@ -33,6 +35,17 @@ export default function Contact() {
           <FaFacebook />
         </a>
       </Socials>
+      <a
+        className="mt-6 h-fit"
+        href={"Osama-Khaled_Front-End-Developer_CV.pdf"}
+        download="Osama-Khaled_Front-End-Developer_CV.pdf"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Button {...HeartBeat}>
+          Get Resume <AiOutlineDownload />
+        </Button>
+      </a>
     </PageWrapper>
   );
 }
@@ -45,4 +58,34 @@ mt-4
 text-2xl
 [&_svg]:text-[#3b3a3a]
 [&_svg:hover]:text-[#08cc7e]
+`;
+
+const HeartBeat = {
+  initial: { scale: 1, boxShadow: "0px 0px 10px 0.1px transparent" },
+  whileHover: {
+    scale: [1, 1.8, 1.8, 1],
+    boxShadow: [
+      "0px 0px 10px 0.1px transparent",
+      "0px 0px 80px 2px #0aff9d",
+      "0px 0px 80px 2px #0aff9d",
+      "0px 0px 15px 0.1px #0aff9d",
+    ],
+  },
+  transition: { duration: 0.2, repeat: Infinity, repeatDelay: 0.6 },
+};
+
+const Button = tw(motion.button)`
+border-[2px]
+border-[#858994]
+font-semibold
+hover:bg-[#0aff9d]
+hover:text-black
+hover:border-transparent
+flex
+sm:p-2
+p-1
+rounded-md
+justify-center
+items-center 
+gap-x-1
 `;
--- src/pages/Home/sections/WhoAmI.jsx
+++ src/pages/Home/sections/WhoAmI.jsx
@@ -6,59 +6,24 @@ import { AiOutlineDownload } from "react-icons/ai";
 export default function WhoAmI() {
   return (
     <Section>
-      <Line className="text-4xl font-bold md:text-7xl">Hi</Line>
-      <p className="underline underline-offset-4">I'm Osama Khaled</p>
-      <Line>
-        a{" "}
-        <Frontend>
-          <div className="absolute w-full block bottom-0 h-1/2 -z-10 bg-[#12a369] right-0 transition-all"></div>
-          front-end
-        </Frontend>{" "}
-        developer.
-      </Line>
-      <a
-        className="sm:mt-6"
-        href={"Osama-Khaled_Front-End-Developer_CV.pdf"}
-        download="Osama-Khaled_Front-End-Developer_CV.pdf"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Button {...HeartBeat}>
-          Get Resume <AiOutlineDownload />
-        </Button>
-      </a>
+      {/* Neon glowing background */}
+      <div className="absolute h-[80%] w-[80%] rounded-full bg-[radial-gradient(circle,#0aff9d_0%,#0aff9dc1_30%,#0aff9d2e_60%,transparent_90%)] blur-3xl -z-10"></div>
+
+      {/* Your text */}
+      <Line className="text-4xl font-bold xl:text-8xl md:text-6xl">Hi</Line>
+      <p className="underline underline-offset-4">I'm Osama</p>
     </Section>
   );
 }
 
-const HeartBeat = {
-  initial: { scale: 1, boxShadow: "0px 0px 10px 0.1px transparent" },
-  whileHover: {
-    scale: [1, 1.6, 1.6, 1],
-    boxShadow: [
-      "0px 0px 10px 0.1px transparent",
-      "0px 0px 80px 2px #0aff9d",
-      "0px 0px 80px 2px #0aff9d",
-      "0px 0px 15px 0.1px #0aff9d",
-    ],
-  },
-  transition: { duration: 0.2, repeat: Infinity, repeatDelay: 0.9 },
-};
-
-const Button = tw(motion.button)`
-border-[2px]
-border-[#858994]
-font-semibold
-hover:bg-[#0aff9d]
-hover:text-black
-hover:border-transparent
-flex
-sm:p-2
+const Frontend = tw.span`
+relative
+text-[#e2e2e2] 
+[&>div]:hover:h-full
+hover:text-[#000000]
+hover:font-semibold
+transition-all
 p-1
-rounded-md
-justify-center
-items-center 
-gap-x-1
 `;
 
 const Section = tw(motion.div)`
@@ -84,17 +49,8 @@ sm:w-fit
 w-[90%]
 who-wrapper
 `;
+
 const Line = tw.h1`
 transition-all
 font-extrabold
-`;
-
-const Frontend = tw.span`
-relative
-text-[#e2e2e2] 
-[&>div]:hover:h-full
-hover:text-[#000000]
-hover:font-semibold
-transition-all
-p-1
 `;
